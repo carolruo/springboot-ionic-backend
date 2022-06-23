3aaa2df0c322c88adcbe91c5d76e94f1e25724dc
Carolina <carolinaruo@gmail.com>
2022-06-23 11:47:13 -0300

    First domain class tested - Category

--- src/main/java/com/carolruo/projeto/resources/CategoryResource.java
+++ src/main/java/com/carolruo/projeto/resources/CategoryResource.java
@@ -1,15 +1,27 @@
 package com.carolruo.projeto.resources;
 
+import com.carolruo.projeto.domain.Category;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoryResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "REST tá funcionando";
+    public List<Category> listar() {
+
+        Category cat1 = new Category(1, "Informatica");
+        Category cat2 = new Category(2, "Escritorio");
+
+        List<Category> list = new ArrayList<>();
+        list.add(cat1);
+        list.add(cat2);
+
+        return list;
     }
 }
