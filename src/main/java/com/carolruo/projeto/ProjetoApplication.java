dc4e39b5c0b6bdba9ababa744b62d49af9c511eb
Carolina <carolinaruo@gmail.com>
2022-06-23 18:03:37 -0300

    populating with dummydata

--- src/main/java/com/carolruo/projeto/ProjetoApplication.java
+++ src/main/java/com/carolruo/projeto/ProjetoApplication.java
@@ -1,13 +1,29 @@
 package com.carolruo.projeto;
 
+import com.carolruo.projeto.domain.Category;
+import com.carolruo.projeto.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class ProjetoApplication {
+public class ProjetoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
