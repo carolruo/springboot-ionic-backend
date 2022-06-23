d2bdc1e5ecc7689a4016f20e778122f70b5f99d4
Carolina <carolinaruo@gmail.com>
2022-06-23 14:37:34 -0300

    Added H2 Database, Spring Data JPA, annotations and Devtools dependencies

--- src/main/java/com/carolruo/projeto/domain/Category.java
+++ src/main/java/com/carolruo/projeto/domain/Category.java
@@ -1,15 +1,23 @@
 package com.carolruo.projeto.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Category implements Serializable {
     private static final long serialVersionUID = 1l;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
 
-    public Category() {}
+    public Category() {
+    }
 
     public Category(Integer id, String name) {
         this.id = id;
