c205b026569c40e728fa77c2e661b0f415ec6aee
Carolina <carolinaruo@gmail.com>
2022-08-16 19:24:35 -0300

    Personalizando retorno da validacao da exception

---
+++ src/main/java/com/carolruo/projeto/resources/exceptions/ValidationError.java
@@ -0,0 +1,23 @@
+package com.carolruo.projeto.resources.exceptions;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+    private static final long serialVersionUID = 1l;
+
+    private List<FieldMessage> fieldMessageList = new ArrayList<>();
+
+    public ValidationError(Integer status, String msg, Long timeStamp) {
+        super(status, msg, timeStamp);
+    }
+
+    public List<FieldMessage> getErrors() {
+        return fieldMessageList;
+    }
+
+    public void addError(String fieldName, String message) {
+        fieldMessageList.add(new FieldMessage(fieldName, message));
+    }
+}
